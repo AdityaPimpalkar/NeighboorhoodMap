58848f2c36b81666ad05e32b5dac73a0a07af1aa
Aditya Pimpalkar <adipimpalkar14@gmail.com>
2017-09-26 20:43:22 +0530

    Create locations.js

---
+++ js/locations.js
@@ -0,0 +1,44 @@
+var locations = [{
+         "title": "St. Pauls Cathedral",
+         "lat": "51.513845",
+         "lng": "-0.098351",
+         "id": "4ac518cef964a520f5a520e3"
+     },
+     {
+         "title": "British Library",
+         "lat": "51.529972",
+         "lng": "-0.127676",
+         "id": "4ac518cef964a52019a620e3"
+     },
+     {
+         "title": "Imperial War Museum",
+         "lat": "51.495831",
+         "lng": "-0.108662",
+         "id": "4ac518d3f964a52078a720e3"
+     },
+     {
+         "title": "Madame Tussauds",
+         "lat": "51.522890",
+         "lng": "-0.154967",
+         "id": "4ac518cef964a520fca520e3"
+     },
+     {
+         "title": "Buckingham Palace",
+         "lat": "51.501364",
+         "lng": "-0.141890",
+         "id": "4abe4502f964a520558c20e3"
+     },
+     {
+         "title": "Sadler's Wells",
+         "lat": "51.529272",
+         "lng": "-0.106325",
+         "id": "4ac518e8f964a52077ab20e3"
+     },
+     {
+         "title": "British Museum",
+         "lat": "51.519413",
+         "lng": "-0.126957",
+         "id": "4ac518d2f964a5203da720e3"
+     }
+
+ ];
